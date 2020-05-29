4a8e539a5a99745babbb477bcea39d5a23b56581
John Yang <47884099+Amazing6312@users.noreply.github.com>
2020-05-29 18:25:09 +0800

    Update dice_loss.py

--- dice_loss.py
+++ dice_loss.py
@@ -1,40 +1,57 @@
 '''
 Author: John Yang from AHU
 Contact: 2268866312@qq.com
-Date: 04.03.2020
 '''
 
 import torch
+import torch.nn as nn
 import numpy as np
 
-def dice_loss(vol1, vol2, labels=None):
-    '''
-    A PyTorch implementation of multi-class version of dice_loss
-
-    parameters:
-    vol1: torch Tensor. The first volume, which shape: [N, C, :, :, :]; N:batch size, C:channel
-    vol2: torch Tensor. The second volume, which sames to vol1
-    labels: default none.
+def make_one_hot(vol, mask):
     '''
 
-    if labels is None:
-        labels = torch.unique(torch.cat((vol1, vol2)))
-    label_no_bg = list()
-    for i in labels:
-        if i != 0:
-            label_no_bg.append(i)  # remove background
+    :param vol: the segmentation map,[N, 1, :, :, :],N: batch size
+    :param mask: can be a python list including all kinds of pixel values of your segmentation map; e.g. mask=[1,2,3]
+    :return: [N, len(mask), :, :, :]
+    '''
 
-    dices = torch.Tensor(np.zeros(len(label_no_bg)))
+    lens = len(mask)
+    shape = np.array(vol.shape)
+    shape[1] = lens
+    shape = tuple(shape)
+    result = torch.zeros(shape)
 
-    for idx, lab in enumerate(label_no_bg):
-        vol1_d = vol1 == lab
-        vol2_d = vol2 == lab
+    for idx, label in enumerate(mask):
+        tmp = vol == label
+        result[:,idx] = tmp
 
-        top = 2 * torch.sum(torch.mul(vol1_d, vol2_d), dtype=float)
+    return result
 
-        bottom = torch.sum(vol1_d, dtype=float) + torch.sum(vol2_d, dtype=float)
-        bottom = torch.max(bottom, (torch.ones_like(bottom, dtype=float) * 1e-5))  # add epsilon.
+class dice_loss(nn.Module):
+    '''
+    vol1,vol2: need to make one hot first
+    '''
+    def __init__(self, epsilon=1e-5):
+        super(dice_loss, self).__init__()
+        self.epsilon = epsilon
 
-        dices[idx] = -1 * (top / bottom)
+    def forward(self, vol1, vol2):
+        shape = vol1.shape
+        total_loss = 0
+        for i in range(shape[1]):
+            top = 2 * torch.sum(torch.mul(vol1[:,i], vol2[:,i]), dtype=float)
 
-    return torch.mean(dices)
+            bottom = torch.sum(vol1[:,i], dtype=float) + torch.sum(vol2[:,i], dtype=float)
+            bottom = torch.max(bottom, (torch.ones_like(bottom, dtype=float) * self.epsilon))  # add epsilon.
+            loss_tmp = -1 * (top / bottom)
+            total_loss += loss_tmp
+
+        return total_loss / shape[1]
+
+def dice_loss_binary(vol1, vol2):
+    top = 2 * torch.sum(torch.mul(vol1, vol2), dtype=float)
+    bottom = torch.sum(vol1, dtype=float) + torch.sum(vol2, dtype=float)
+    bottom = torch.max(bottom, (torch.ones_like(bottom, dtype=float) * 1e-5))  # add epsilon.
+    loss = -1 * (top / bottom)
+
+    return loss
